ebc7a17d8020dc954a4e81d2698f19a8cba9181d
Ruben Broekx <broekxruben@gmail.com>
2021-07-18 17:53:07 +0200

    Finished the notebook

--- src/few_shot_face_classification/__init__.py
+++ src/few_shot_face_classification/__init__.py
@@ -1,1 +1,4 @@
 """Few-Shot Face Classification package."""
+from few_shot_face_classification.main import add_none, detect_and_export, recognise, validate_labels
+
+__all__ = ["add_none", "detect_and_export", "recognise", "validate_labels"]
